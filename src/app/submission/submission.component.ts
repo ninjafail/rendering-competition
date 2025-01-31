f572d8bf7a193c66440e73e653efce9450320ac6
Leonie von Mann <s8levonm@stud.uni-saarland.de>
2025-01-31 14:51:39 +0100

    Add content to submission page

--- src/app/submission/submission.component.ts
+++ src/app/submission/submission.component.ts
@@ -1,9 +1,10 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { SectionComponent } from '../components/section/section.component';
+import { FigureComponent } from '../components/figure/figure.component';
 
 @Component({
   selector: 'app-submission',
-  imports: [SectionComponent],
+  imports: [SectionComponent, FigureComponent],
   templateUrl: './submission.component.html',
   styleUrl: './submission.component.scss'
 })
